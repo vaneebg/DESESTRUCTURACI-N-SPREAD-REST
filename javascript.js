2f9c9472880cff0e318d0038befe91db1fea7ed4
Vanesa <vaneebg@gmail.com>
2022-05-03 20:40:19 +0200

    hecho y comprobado extra

--- javascript.js
+++ javascript.js
@@ -84,22 +84,30 @@ const { yesterday, today: maximaHoy, tomorrow: maximaManana } = HIGH_TEMPERATURE
 console.log(maximaHoy)
 console.log(maximaManana)
 
+
+
 // Escriba una función llamada onlyUniques que acepte cualquier número de argumentos y devuelva un array de elementos únicos, sin repetidos.
-// onlyUniques('gato', 'pollo', 'cerdo', 'cerdo'); 
+const onlyUniques = (...args) => { const ArrayUnica = new Set(args); return ArrayUnica }
+console.log(onlyUniques('gato', 'pollo', 'cerdo', 'cerdo'));
 // //['gato', 'pollo', 'cerdo']
-// onlyUniques(1, 1, 2, 2, 3, 6, 7, 8); //[1, 2, 3, 6, 7, 8]
+console.log(onlyUniques(1, 1, 2, 2, 3, 6, 7, 8)); //[1, 2, 3, 6, 7, 8]
+
+
 
 // Escriba una función llamada combineAllArrays que pueda recibir cualquier cantidad de arrays como argumentos y los combine todos en un solo array.
-// combineAllArrays([3, 6, 7, 8],[2, 7, 3, 1])
-// // [3, 6, 7, 8, 2, 7, 3, 1]
-// combineAllArrays([2, 7, 3, 1],[2, 7, 4, 12],[2, 44, 22, 7, 3, 1]);
+const combineAllArrays = (...args) => { const concatenarArray = [...args]; return concatenarArray }
+console.log(combineAllArrays([3, 6, 7, 8], [2, 7, 3, 1]))
+    // // [3, 6, 7, 8, 2, 7, 3, 1]
+console.log(combineAllArrays([2, 7, 3, 1], [2, 7, 4, 12], [2, 44, 22, 7, 3, 1]));
 // // [2, 7, 3, 1, 2, 7, 4, 12, 2, 44, 22, 7, 3, 1]
+
+
 
 // Escriba una función llamada sumAndSquare que reciba cualquier número de argumentos, los eleve al cuadrado y devuelva la suma de todos los valores cuadrados.
 
-// const sumAndSquare = (...sumAndSquare) => {
-//     const numbersElevado = sumAndSquare.map(number => { return number ** 2 })
-
-
-// }
-// console.log(sumAndSquare(2, 3))
+const sumAndSquare = (...numbers) => {
+    const Elevar = numbers.map(number => number ** 2);
+    const sum = Elevar.reduce((a, b) => a + b);
+    return sum
+}
+console.log(sumAndSquare(2, 3, 4))
